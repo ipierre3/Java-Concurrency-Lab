48150c643a4ddb020fe848d039b59311fa129a6f
Ignace Pierre <ipierre354@gmail.com>
2022-11-10 16:45:51 -0500

    introduced for loop for 10 iterations of method

--- src/Simulation.java
+++ src/Simulation.java
@@ -1,8 +1,17 @@
 public class Simulation {
     public void runSim(){
         System.out.println("Running simulation");
+
         Account myAccount = new Account(1000);
 
-        myAccount.withdraw(200);
+        for (int i = 0; i < 10; i++) {
+            myAccount.withdraw(100);
+        }
+        for (int i = 0; i < 10; i++) {
+            myAccount.withdraw(100);
+        }
+
+        System.out.println("Simulation complete");
     }
+
 }
